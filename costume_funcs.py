09d030f2405747ceddc3b0a41498d5459b953d26
snirvt <snirvt@gmail.com>
2022-03-17 05:16:43 +0200

    added try except to the cost function

--- costume_funcs.py
+++ costume_funcs.py
@@ -25,18 +25,21 @@ def mse():
     return mse
 
 def _mse_split_mean(X_y, splitting_cond, w): # gp.fit(X, X_y)
-    left_y = X_y[splitting_cond<0]['Y'].values 
-    right_y = X_y[splitting_cond>=0]['Y'].values
-    if len(left_y) == 0 or len(right_y) == 0:
+    try:
+        left_y = X_y[splitting_cond<0]['Y'].values 
+        right_y = X_y[splitting_cond>=0]['Y'].values
+        if len(left_y) == 0 or len(right_y) == 0:
+            return float('inf')
+        mean_left_y = np.mean(left_y)
+        mean_right_y = np.mean(right_y)
+        y_true = np.concatenate((left_y, right_y), axis=None)
+        y_pred = np.concatenate((np.repeat(mean_left_y, len(left_y)),
+                                np.repeat(mean_right_y, len(right_y))),
+                                axis=None)
+        mse_split = mean_squared_error(y_true, y_pred)
+        return mse_split
+    except:
         return float('inf')
-    mean_left_y = np.mean(left_y)
-    mean_right_y = np.mean(right_y)
-    y_true = np.concatenate((left_y, right_y), axis=None)
-    y_pred = np.concatenate((np.repeat(mean_left_y, len(left_y)),
-                            np.repeat(mean_right_y, len(right_y))),
-                            axis=None)
-    mse_split = mean_squared_error(y_true, y_pred)
-    return mse_split
 
 def mse_split_mean():
     mse_split_mean = make_fitness(_mse_split_mean, greater_is_better=False)
